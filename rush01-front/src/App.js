df64be992d88704d75307d86408e13796ed5b8e1
from97 <skm1104@daum.net>
2021-08-30 15:17:29 +0900

    directory

--- rush01-front/src/App.js
+++ rush01-front/src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body, ul, li, input, button, a {
+    all: unset;
+    list-style: none;
+  }
+  a, button {
+    cursor: pointer;
+  }
+  #root {
+    width: 100%;
+    height: 100%;
+    font-family: 'Noto Sans', sans-serif;
+  }
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
---
+++ rush01-front/src/pages/JoinPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const JoinPageStyled = styled.div``;
+
+function JoinPage() {
+  return <JoinPageStyled></JoinPageStyled>;
+}
+
+export default JoinPage;
