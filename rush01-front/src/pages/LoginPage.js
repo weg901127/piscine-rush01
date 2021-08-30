ae41ca4e5ad3d30655db66a5c8711fd80af5ad9c
from97 <skm1104@daum.net>
2021-08-30 15:16:47 +0900

    directory

---
+++ rush01-front/src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginPageStyled = styled.div``;
+
+function LoginPage() {
+  return <LoginPageStyled></LoginPageStyled>;
+}
+
+export default LoginPage;
