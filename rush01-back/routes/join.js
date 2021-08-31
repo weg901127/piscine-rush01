f1d45dfd3f3da050189c5b68eb0a0e0300672e4d
gilee <gilee@student.42seoul.kr>
2021-08-31 20:20:35 +0900

    img

--- rush01-back/routes/join.js
+++ rush01-back/routes/join.js
@@ -10,7 +10,7 @@ router.post('/',
   ensureLoggedIn('/api/login/42'),//upload.single("file"),
   function (req, res) {
     Users.update({nickname:req.body.nickname},{ where: { username: req.user.username } }).then((results) => {
-         res.redirect('/')
+         res.status(200).json({msg:"done"});
     }).catch((err) => { console.log('' + err.stack); });
     //res.render('index', {name : req.user.username});
   });
