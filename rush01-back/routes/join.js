d65806da38951d7847c364e97c2cd0fd7abb04f3
gilee <gilee@student.42seoul.kr>
2021-08-31 20:15:40 +0900

    img

--- rush01-back/routes/join.js
+++ rush01-back/routes/join.js
@@ -3,39 +3,15 @@ var router = express.Router();
 var { sequelize, Users } = require('../models');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 var passport = require('../passportset');
-const multer = require('multer');
 router.use(passport.initialize());
 router.use(passport.session());
-// const storage = multer.diskStorage({
-//     destination: "./public/img/",
-//     filename: function(req, file, cb) {
-//       cb(null, "imgfile" + Date.now());
-//     }
-//   });
-//   const upload = multer({
-//     storage: storage,
-//     limits: { fileSize: 10000000000 }
-//   });
-router.get('/',
-  ensureLoggedIn('/api/login/42'),
-  function (req, res) {
-    Users.findAll({ where: { username: req.user.username }}).then((results) => {
-          res.json(results)
-    }).catch((err) => { console.log('' + err); });
-    //res.render('index', {name : req.user.username});
-  });
+
 router.post('/',
   ensureLoggedIn('/api/login/42'),//upload.single("file"),
   function (req, res) {
-    try{
-      var buf = new Buffer(req.body.photo.toString('binary'),'binary')
-      console.log(buf)
-    }catch(e){
-      console.log(e);
-    }
-    //Users.update({nickname:req.body.nickname,photo:req.body.photo},{ where: { username: req.user.username } }).then((results) => {
-    //      res.redirect('/')
-    //}).catch((err) => { console.log('' + err.stack); });
+    Users.update({nickname:req.body.nickname},{ where: { username: req.user.username } }).then((results) => {
+         res.redirect('/')
+    }).catch((err) => { console.log('' + err.stack); });
     //res.render('index', {name : req.user.username});
   });
 
